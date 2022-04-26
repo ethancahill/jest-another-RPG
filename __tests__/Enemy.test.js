14e47e8b24e6f6e27aa3eaa787f80ceea5632dd9
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-04-25 22:36:09 -0400

    added Enemy constructor with test

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('@jest/globals');
+const Enemy = require('../lib/Enemy')
+const Potion = require('../lib/Potion')
+
+jest.mock('../lib/Potion.js');
+
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
+
