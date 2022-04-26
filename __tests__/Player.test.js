33ee029de95aab14682b866c1115bb64ce3137d7
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-04-25 20:29:13 -0400

    added potion array to player constructor with test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,8 @@
+const Potion = require('../lib/Potion');
+const Player = require('../lib/Player');
+const { expect } = require('@jest/globals');
 
-const Player = require('../lib/Player')
+jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -8,4 +11,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
