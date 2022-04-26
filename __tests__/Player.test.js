dcd299a6b3091ead543e6897dc01bdb664d024d6
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-04-25 21:10:47 -0400

    added getStats and getInventory functions for player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,6 +2,7 @@ const Potion = require('../lib/Potion');
 const Player = require('../lib/Player');
 const { expect } = require('@jest/globals');
 
+
 jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
@@ -14,4 +15,25 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
